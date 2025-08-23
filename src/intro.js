4c16ba979f8cc44ca6d9979c9b3f5664bb51764a
Hugo Manzoni <hugocmdesign@gmail.com>
2025-08-23 16:38:30 +0200

    implement new fn, refactor old ones and create test cases for all of them

--- src/intro.js
+++ src/intro.js
@@ -1,8 +1,8 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  if (a > b) return a;
-  else if (b > a) return b;
-  return a;
+  // if (a > b) return a;
+  // else if (b > a) return b;
+  return (b > a) ? b : a;
 }
 
 // Exercise
@@ -11,4 +11,16 @@ export function fizzBuzz(n) {
   if (n % 3 === 0) return 'Fizz';
   if (n % 5 === 0) return 'Buzz';
   return n.toString();
+}
+
+export function calculateAvg(numbers) {
+  if (numbers.length === 0) return NaN;
+  const sum = numbers.reduce((sum, current) => sum + current, 0);
+  return sum/numbers.length;
+}
+
+export function factorial(n) {
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n-1);
 }
