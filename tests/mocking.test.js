956357266867ca6e52d79ec3f7df29c940799e70
Hugo Manzoni <hugocmdesign@gmail.com>
2025-08-26 17:42:29 +0200

    interaction testing

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,30 +1,47 @@
 import { describe, expect, vi, it } from "vitest";
-import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
+import {
+  getPriceInCurrency,
+  getShippingInfo,
+  renderPage,
+} from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
 import { getShippingQuote } from "../src/libs/shipping";
+import { trackPageView } from "../src/libs/analytics";
 
 // Hoisting
-vi.mock('../src/libs/currency');
-vi.mock('../src/libs/shipping');
+vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
+vi.mock("../src/libs/analytics");
 
-describe('getPriceInCurrency', () => {
-    it('should return price in target currency', () => {
-        vi.mocked(getExchangeRate).mockReturnValue(1.5);
-        const price = getPriceInCurrency(10, 'AUD');
-        expect(price).toBe(15);
-    });
+describe("getPriceInCurrency", () => {
+  it("should return price in target currency", () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+    const price = getPriceInCurrency(10, "AUD");
+    expect(price).toBe(15);
+  });
 });
 
-describe('getShippingInfo', () => {
-    it('should return shipping unavailable if quote cannot be fetched', () => {
-        vi.mocked(getShippingQuote).mockReturnValue(null);
-        const result = getShippingInfo('Argentina');
-        expect(result).toMatch(/unavailable/i);
-    });
-    it('should return shipping info if quote can be fetched', () => {
-        vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
-        const result = getShippingInfo('Argentina');
-        expect(result).toMatch('$10');
-        expect(result).toMatch(/2 days/i);
-    });
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+    const result = getShippingInfo("Argentina");
+    expect(result).toMatch(/unavailable/i);
+  });
+  it("should return shipping info if quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+    const result = getShippingInfo("Argentina");
+    expect(result).toMatch("$10");
+    expect(result).toMatch(/2 days/i);
+  });
+});
+
+describe("renderPage", () => {
+  it("should return correct content", async () => {
+    const result = await renderPage();
+    expect(result).toMatch(/content/i);
+  });
+  it("should call analytics", async () => {
+    await renderPage();
+    expect(trackPageView).toBeCalledWith('/home');
+  });
 });
